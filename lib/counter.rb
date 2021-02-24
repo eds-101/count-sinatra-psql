674c968462620c18f8927cf376c6069556655abf
Eds <steadyede@live.co.uk>
2021-02-24 09:18:14 +0000

    Last updated feature

--- lib/counter.rb
+++ lib/counter.rb
@@ -9,11 +9,13 @@ class Counter
   def increment
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count + 1}' WHERE id=1;")
+    update_timestamp
   end
 
   def decrement
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count - 1}' WHERE id=1;")
+    update_timestamp
   end
 
   def update_timestamp
